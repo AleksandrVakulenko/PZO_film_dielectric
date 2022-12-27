b225cb357df56463b165e05ba15a46ef8b909041
Aleksandr Vakulenko <sasha705@mail.ru>
2022-12-27 15:30:26 +0300

    add dielectric math matlab scripts

---
+++ Matlab/dielectric_math/eps_from_Rs_C_Rp.m
@@ -0,0 +1,68 @@
+clc
+
+freq = 10.^(linspace(log10(20), log10(300000), 30));
+
+surf = pi*(70e-6/2)^2; %m^2
+h = 100e-9; %m
+eps0 = 8.85e-12; %F/m
+cap0 = eps0*surf/h;
+
+R_series = 50000; %Ohm
+R_parallel = 100e6; %Ohm
+C = 1e-9; %F
+X = 1./(2*pi*i*freq*C);
+
+
+% z_film = R_series + X; %series
+% z_film = R.*X./(R + X); %parallel
+
+
+z_film = X;
+
+z_full = R_parallel.*z_film./(R_parallel + z_film);
+z_full = R_series + z_full;
+
+
+z1 = real(z_full);
+z2 = imag(z_full);
+
+
+eps1 = -z2./(2*pi*cap0*freq.*(z1.^2+z2.^2));
+eps2 =  z1./(2*pi*cap0*freq.*(z1.^2+z2.^2));
+
+
+
+
+figure('position', [220 326 1116 650])
+
+subplot(2,2,1)
+plot(freq, z1)
+ylabel('z''')
+set(gca, 'xscale', 'log')
+
+subplot(2,2,3)
+plot(freq, z2)
+ylabel('z"')
+set(gca, 'xscale', 'log')
+
+
+subplot(2,2,2)
+plot(freq, eps1)
+ylabel('eps''')
+% ylim([0 3000])
+set(gca, 'xscale', 'log')
+
+subplot(2,2,4)
+plot(freq, eps2)
+ylabel('eps"')
+% xlim("manual")
+% ylim([0 1500])
+set(gca, 'xscale', 'log')
+
+
+
+
+
+
+
+
